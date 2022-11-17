8ec7966b99dffc0209da806f12aadeb4f1b66ab6
Zakaria Narjis <zakaria.narjis.97@gmail.com>
2022-11-17 21:41:33 +0100

    add try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,20 +23,22 @@ class Streamlitapp:
             self.display_analysis(username)
 
     def generate_analysis(self,username):
-        with st.spinner('Fetching tweets...'):
-            output=self.twitter_app.fetch_tweets_replies(username)
-        if output==0:
+        try:
+            with st.spinner('Fetching tweets...'):
+                output=self.twitter_app.fetch_tweets_replies(username)
+            if output==0:
+                st.error('No user or tweets was found with this username.', icon="🚨")
+                return None,None
+            elif output==1:
+                st.error('No replies was found to the user\'s tweets.', icon="🚨")
+                return None,None
+            else:
+                user_tweets,tweets_replies=output   
+                with st.spinner('Analyzing replies sentiments...'):
+                    tweets_sentiments=self.predictor.predict_sentiment(tweets_replies)
+                return tweets_sentiments,1
+        except:
             st.error('No user or tweets was found with this username.', icon="🚨")
-            return None,None
-        elif output==1:
-            st.error('No replies was found to the user\'s tweets.', icon="🚨")
-            return None,None
-        else:
-            user_tweets,tweets_replies=output   
-            with st.spinner('Analyzing replies sentiments...'):
-                tweets_sentiments=self.predictor.predict_sentiment(tweets_replies)
-            return tweets_sentiments,1
-
     def generate_tweets_stats(self,df):
         tweets_stats=df.copy()
         tweets_stats=tweets_stats[['conversation_id','positive','negative']].groupby(['conversation_id']).sum().reset_index()       
